cb77cedc86468e9ce3b912b17efaa40282b0964f
tungnd <nguyenduongtungptot@gmail.com>
2018-10-12 08:41:25 +0700

    commit controller

--- src/main/java/com/controller/ExportController.java
+++ src/main/java/com/controller/ExportController.java
@@ -1,6 +1,7 @@
 package com.controller;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.util.List;
 
@@ -21,8 +22,6 @@ import com.util.Constant;
 @Controller
 public class ExportController {
 	
-	private static final String CSV_HEADER = "Danh sách thông tin thẻ bảo hiểm";
-	
 	@Autowired
 	private UserService userService;
 	
@@ -31,72 +30,91 @@ public class ExportController {
 	
 	
 	@RequestMapping("/export")
-	public void downloadCsvFile(HttpServletResponse response, HttpSession session) {
-		response.setContentType("text/csv");
-		response.setHeader("Content-Disposition", "attachment; filename=\"my-csv-file.csv\"");
-		PrintWriter printWriter = null;
-		int companyIdSelected = (int) session.getAttribute("company_id_selected");
-		String userFullName = (String) session.getAttribute("user_full_name");
-		String insuranceNumber = (String) session.getAttribute("insurance_number");
-		String placeOfRegister = (String) session.getAttribute("place_of_register");
+	public void downloadCsvFile(HttpServletResponse response, HttpSession session) throws IOException {
+		OutputStream outputStream = null;
+		StringBuilder contents = new StringBuilder();
+		int companyIdSelected;
+		String userFullName = "";
+		String insuranceNumber = "";
+		String placeOfRegister = "";
+		if (session.getAttribute("company_id_selected") == null) {
+			List<Company> companyList = companyService.findAllCompany();
+			companyIdSelected = companyList.get(0).getCompanyInternalId();
+		} else {
+			companyIdSelected = (int) session.getAttribute("company_id_selected");
+			userFullName = (String) session.getAttribute("user_full_name");
+			insuranceNumber = (String) session.getAttribute("insurance_number");
+			placeOfRegister = (String) session.getAttribute("place_of_register");
+		}
 		List<User> userList = userService.getUserDataToExport(companyIdSelected, userFullName, insuranceNumber,
 				placeOfRegister);
 		Company company = companyService.findCompanyById(companyIdSelected);
-		try {
-			printWriter = response.getWriter();
-			printWriter.append(CSV_HEADER);
-			printWriter.append("\n");
-			printWriter.append("Tên công ty");
-			printWriter.append(",");
-			printWriter.append(company.getCompanyName());
-			printWriter.append("\n");
-			printWriter.append("Địa chỉ");
-			printWriter.append(",");
-			printWriter.append(company.getAddress());
-			printWriter.append("\n");
-			printWriter.append("Số điện thoại");
-			printWriter.append(",");
-			printWriter.append(company.getTelephone());
-			printWriter.append("\n\n");
-			printWriter.append("Họ và tên");
-			printWriter.append(",");
-			printWriter.append("Giới tính");
-			printWriter.append(",");
-			printWriter.append("Ngày sinh");
-			printWriter.append(",");
-			printWriter.append("Mã số thẻ bảo hiểm");
-			printWriter.append(",");
-			printWriter.append("Ngày bắt đầu");
-			printWriter.append(",");
-			printWriter.append("Ngày kết thúc");
-			printWriter.append(",");
-			printWriter.append("Nơi đăng ký KCB");
-			printWriter.append("\n");
-			for (User user : userList) {
-				printWriter.append(user.getUserFullName());
-				printWriter.append(",");
-				if (user.getUserSexDivision() == Constant.GENDER.FEMALE.getCode()) {
-					printWriter.append(Constant.GENDER.FEMALE.getGender());
-				} else {
-					printWriter.append(Constant.GENDER.MALE.getGender());
-				}
-				printWriter.append(",");
-				printWriter.append(Common.formatDate(user.getBirthDate()));
-				printWriter.append(",");
-				printWriter.append(user.getInsurance().getInsuranceNumber());
-				printWriter.append(",");
-				printWriter.append(Common.formatDate(user.getInsurance().getInsuranceStartDate()));
-				printWriter.append(",");
-				printWriter.append(Common.formatDate(user.getInsurance().getInsuranceEndDate()));
-				printWriter.append(",");
-				printWriter.append(user.getInsurance().getPlaceOfRegister());
-				printWriter.append("\n");
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			printWriter.flush();
-			printWriter.close();
+		response.setContentType("text/csv");
+		response.setHeader("Content-Disposition", "attachment; filename=\"my-csv-file.csv\"");
+		outputStream = response.getOutputStream();
+		contents.append("Danh sách thông tin thẻ bảo hiểm");
+		contents.append("\n\n");
+		contents.append("Tên công ty");
+		contents.append(",");
+		contents.append(company.getCompanyName());
+		contents.append("\n");
+		contents.append("Địa chỉ");
+		contents.append(",");
+		contents.append(company.getAddress());
+		contents.append("\n");
+		contents.append("Email");
+		contents.append(",");
+		if (Common.checkStringEmptyOrNull(company.getEmail()) == true) {
+			contents.append(company.getEmail());
 		}
+		contents.append("\n");
+		contents.append("Số điện thoại");
+		contents.append(",");
+		if (Common.checkStringEmptyOrNull(company.getTelephone()) == true) {
+			contents.append(company.getTelephone());
+		}
+		contents.append("\n\n");
+		contents.append("Họ và tên");
+		contents.append(",");
+		contents.append("Giới tính");
+		contents.append(",");
+		contents.append("Ngày sinh");
+		contents.append(",");
+		contents.append("Mã số thẻ bảo hiểm");
+		contents.append(",");
+		contents.append("Ngày bắt đầu");
+		contents.append(",");
+		contents.append("Ngày kết thúc");
+		contents.append(",");
+		contents.append("Nơi đăng ký KCB");
+		contents.append("\n");
+		for (User user : userList) {
+			contents.append(user.getUserFullName());
+			contents.append(",");
+			if (user.getUserSexDivision() == Constant.GENDER.FEMALE.getCode()) {
+				contents.append(Constant.GENDER.FEMALE.getGender());
+			} else {
+				contents.append(Constant.GENDER.MALE.getGender());
+			}
+			contents.append(",");
+			if (user.getBirthDate() != null) {
+				contents.append(Common.formatDate(user.getBirthDate()));
+			}
+			contents.append(",");
+			contents.append(user.getInsurance().getInsuranceNumber());
+			contents.append(",");
+			contents.append(Common.formatDate(user.getInsurance().getInsuranceStartDate()));
+			contents.append(",");
+			contents.append(Common.formatDate(user.getInsurance().getInsuranceEndDate()));
+			contents.append(",");
+			contents.append(user.getInsurance().getPlaceOfRegister());
+			contents.append("\n");
+		}
+		outputStream.write(239);
+		outputStream.write(187);
+		outputStream.write(191);
+		outputStream.write(contents.toString().getBytes("UTF-8"));
+		outputStream.flush();
+		outputStream.close();
 	}
 }
